1262e1014e774d05b8a6361574469ca0451cd3cb
Ricardo Montesinos <richimf1@gmail.com>
2019-12-11 23:27:51 +0100

    Sesion-07

---
+++ Sesion-07/Ejemplo-02/PostExample.playground/Contents.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+let config = URLSessionConfiguration.default
+
+let session = URLSession(configuration: config)
+
+let url = URL(string: "https://httpbin.org/anything")!
+
+
+var request = URLRequest(url: url)
+request.httpMethod = "POST"
+request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+// your post request data
+let postDict : [String: Any] = ["name": "Bedu",
+                                "favorite_animal": "Chihuahua"]
+
+func postdata() {
+  guard let postData = try? JSONSerialization.data(withJSONObject: postDict, options: []) else {
+    return
+  }
+  
+  request.httpBody = postData
+}
+postdata()
+
+let task = session.dataTask(with: request) { data, response, error in
+  
+    // ensure there is no error for this HTTP response
+    guard error == nil else {
+        print ("error: \(error!)")
+        return
+    }
+  
+    // ensure there is data returned from this HTTP response
+    guard let content = data else {
+        print("No data")
+        return
+    }
+  
+    // serialise the data / NSData object into Dictionary [String : Any]
+    guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) as? [String: Any] else {
+        print("Not containing JSON")
+        return
+    }
+  
+    print("gotten json response dictionary is \n \(json)")
+    // update UI using the response here
+}
+
+// execute the HTTP request
+task.resume()
+
+

96dc483c5a88115ff36542d4e25d9baa494aaa3c
Ricardo Montesinos <richimf1@gmail.com>
2019-12-11 23:54:37 +0100

    Sesion-07

--- Sesion-07/Ejemplo-02/PostExample.playground/Contents.swift
+++ Sesion-07/Ejemplo-02/PostExample.playground/Contents.swift
@@ -1,11 +1,8 @@
 import UIKit
 
 let config = URLSessionConfiguration.default
-
 let session = URLSession(configuration: config)
-
-let url = URL(string: "https://httpbin.org/anything")!
-
+let url = URL(string: "https://....")!
 
 var request = URLRequest(url: url)
 request.httpMethod = "POST"
@@ -14,15 +11,13 @@ request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 // your post request data
 let postDict : [String: Any] = ["name": "Bedu",
                                 "favorite_animal": "Chihuahua"]
-
-func postdata() {
+func generateBody() {
   guard let postData = try? JSONSerialization.data(withJSONObject: postDict, options: []) else {
     return
   }
   
   request.httpBody = postData
 }
-postdata()
 
 let task = session.dataTask(with: request) { data, response, error in
   
@@ -49,6 +44,7 @@ let task = session.dataTask(with: request) { data, response, error in
 }
 
 // execute the HTTP request
+generateBody()
 task.resume()
 
 

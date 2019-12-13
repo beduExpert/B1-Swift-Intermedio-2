30757b7551ae06406abaf09017e1a4b22ec362bc
Ricardo Montesinos <richimf1@gmail.com>
2019-12-13 17:44:55 +0100

    Ejemplo-02

---
+++ Sesion-08/Ejemplo-02/MVVMApp/MVVMApp/MVVM.swift
@@ -0,0 +1,27 @@
+//
+//  MVVM.swift
+//  MVVMApp
+//
+//  Copyright © 2019 Bedu. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel {
+  
+  func validate(user: String, password: String) -> Bool {
+    // Validation code...
+    return true
+  }
+  
+  func download(url: String, completion: @escaping (UIImage) -> Void) {
+    guard let url = URL(string: url) else { return }
+    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+      if error == nil, let imageData = data {
+        completion(UIImage(data: imageData)!)
+      }
+    }
+    task.resume()
+  }
+  
+}
---
+++ Sesion-08/Ejemplo-02/MVVMApp/MVVMApp/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  MVVMApp
+//
+//  Copyright © 2019 Bedu. All rights reserved.
+//
+
+import Foundation
